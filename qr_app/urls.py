442308c1abd9211e7804c24b6fc7721dc5f27790
judom19 <judom19@gmail.com>
2023-11-02 19:36:01 -0600

    agreagando vistas para mostrar la lista de los codigos generados y rurls con configuraiciones adicionales

---
+++ qr_app/urls.py
@@ -0,0 +1,15 @@
+# qr_app/urls.py
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name = 'qr_app'
+urlpatterns = [
+    path('', views.generate_qr, name='generate_qr'),
+    path('show/<int:qr_id>/', views.show_qr, name='show_qr'),
+    path('qr_list', views.qr_list, name='qr_list'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- qr_app/views.py
+++ qr_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.core.files.base import ContentFile
 import qrcode
 from qr_app.models import QRCode
-from PIL import Image
 from io import BytesIO
+
 
 # funcion encargada de generar el codigo QR 
 def generate_qr(request):
@@ -17,10 +16,10 @@ def generate_qr(request):
         
         # se crea un objeto QRCode
         qr = qrcode.QRCode(
-            version = 1,
-            error_correction = qrcode.constants.ERROR_CORRRECT_L,
-            box_size = 10,
-            border = 4,            
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,            
         )
         # se agrega el texto al objeto  QRCode
         qr.add_data(text)
@@ -29,7 +28,7 @@ def generate_qr(request):
         qr.make(fit=True)
         
         # se crea una imagen a partir del codigo QR
-        img = qr.make_make(fill_color='black', back_color='white')
+        img = qr.make_image(fill_color="black", back_color="white")
         
         # se guarda el codigo QR en el modelo 'QRCode' de 'qr_app' y se crea un objeto QRCode con el texto recibido
         qr_code = QRCode(text=text)
@@ -47,10 +46,11 @@ def generate_qr(request):
         qr_code.save()
         
         # se redirecciona a al vista de visualizacion del codigo QR 
-        return redirect ('show_qr', qr_id=qr_code.id)
+        return redirect ('qr_app:show_qr', qr_id=qr_code.id)
     
     # si la peticion no es de tipo POST, entonces se la pagina de generacion de codigos QR
     return render(request,'qr_app/generate_qr.html')
+
 
 # funcion encargada de mostrar un codigo QR a partir de su ID
 def show_qr(request, qr_id):
@@ -59,3 +59,15 @@ def show_qr(request, qr_id):
     
     # se devuelve la pagina de visualizacion del codigo QR  con el codigo QR cargado
     return render(request, 'qr_app/show_qr.html', {'qr_code': qr_code})
+
+
+# vista encargada de mostrar la lista de todos lo QR generados
+def qr_list(request):
+    qr_codes = QRCode.objects.all()
+    
+    context = {
+        'qr_codes':qr_codes,
+    }
+    
+    return render(request,'qr_app/list_qr.html',context)
+
